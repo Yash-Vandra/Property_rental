dd245770823d64c174d8aceb90bee4ce6696b9e9
Yash-Vandra <yashvandra10@gmail.com>
2023-12-13 12:36:55 +0530

    [add] rental property

---
+++ __manifest__.py
@@ -0,0 +1,5 @@
+{
+    'name': "Rental Property",
+    'data': [
+    ],
+}
---
+++ models/services.py
@@ -0,0 +1,7 @@
+from odoo_16.odoo import models,fields
+
+class Services(models.Model):
+    _name = 'services.services'
+    _description = "The Properties Provided On Rent"
+
+    
